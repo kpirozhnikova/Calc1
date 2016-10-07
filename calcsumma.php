e15e84721e7feca236f0dd71bcbaa5350dcaeda5
Marina1303 <pupina3@mail.ru>
2016-10-07 17:51:00 +0700

    Summator

---
+++ calcsumma.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+</head>
+<body>
+<form method='GET'>
+<input type="text" name="arg1" value="">
+<input type="text" name="arg2" value="">
+<input type="submit" value="Сумма">
+</form>
+<?php
+if (isset($_GET['arg1']) && (isset($_GET['arg2']))) {
+	$a=$_GET['arg1'];
+	$b=$_GET['arg2'];
+	$c=intval($_GET['arg1']) + intval($_GET['arg2']);
+	echo "<div>".$a. "+" .$b. "="  .$c. "</div>";
+}
+?>
+</body>
+</html>
